1e49cfec30411b7521b80f90b2d919d72b084abd
Duken Marga <dukenmarga@gmail.com>
2024-02-14 16:00:47 +0700

    Return uuid to json

--- main.go
+++ main.go
@@ -4,13 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
+
+type Document struct {
+	Latex string `uri:"latex" binding:"required"`
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/api/v1/simple", func(c *gin.Context) {
+	r.GET("/api/v1/simple/:latex", func(c *gin.Context) {
+		var doc Document
+		if err := c.ShouldBindUri(&doc); err != nil {
+			c.JSON(400, gin.H{"msg": err})
+			return
+		}
+		id := uuid.New()
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"id":    id.String(),
+			"latex": doc.Latex,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
