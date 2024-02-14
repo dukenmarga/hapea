6ccdc983f445c25c11340698f030a23bbd6ffadb
Duken Marga <dukenmarga@gmail.com>
2024-02-14 15:26:12 +0700

    Add simple routing to show json

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/api/v1/simple", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
